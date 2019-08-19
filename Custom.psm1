74a7a22f18faac12a84168f78b79d723388179d9
Horace Ko <horace.ko@gmail.com>
2019-08-18 20:40:50 -0700

    Fix bugs in caps lock and ctrl switching.

--- Custom.psm1
+++ Custom.psm1
@@ -1,11 +1,11 @@
 Function EnableCapsRemapToCtrl {
     Write-Output "Remapping Caps Lock to Left Ctrl"
-    $hexified = "00,00,00,00,00,00,00,00,03,00,00,00,1d,00,3a,00,3a,00,1d,00,00,00,00,00".Split(",") | % { "0x$_"};
+    $hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(",") | % { "0x$_"};
     $kbLayout = 'HKLM:\SYSTEM\CurrentControlSet\Control\Keyboard Layout';
     If (!(Test-Path $kbLayout)) {
 		New-Item -Path $kbLayout -Force | Out-Null
     }
-    Set-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified)
+    Set-ItemProperty -Path $kbLayout -Name "Scancode Map" -Type Binary -Value ([byte[]]$hexified)
 }
 
 Function DisableCapsRemapToCtrl {
@@ -15,11 +15,13 @@ Function DisableCapsRemapToCtrl {
 }
 
 Function InstallUsefulMsftSoftware {
+    Write-Output "Installing Photos and Get Started"
 	Get-AppxPackage -AllUsers "Microsoft.Windows.Photos" | ForEach-Object {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 	Get-AppxPackage -AllUsers "Microsoft.Getstarted" | ForEach-Object {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 }
 
 Function UninstallUsefulMsftSoftware {
+    Write-Output "Uninstalling Photos and Get Started"
 	Get-AppxPackage "Microsoft.Windows.Photos" | Remove-AppxPackage
 	Get-AppxPackage "Microsoft.Getstarted" | Remove-AppxPackage
 }
